2091912e9ae103c4c7cd25fbdcd44f4d9919b4e1
SudeepMi <sudeepm0599@gmail.com>
2023-11-02 00:53:16 +0545

    lazy load

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import "react-activity/dist/library.css";
 import { members } from "./members";
 import 'remixicon/fonts/remixicon.css'
 import { toast } from 'react-toastify';
-
+import { LazyLoadImage } from "react-lazy-load-image-component"
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 
 function App() {
@@ -29,17 +30,17 @@ function App() {
   let photoActive = {};
 
 
-  const url = "https://backend-poll.onrender.com/api";
-  // const url = "http://localhost:3000/api";
+  // const url = "https://backend-poll.onrender.com/api";
+  const url = "http://localhost:3000/api";
 
-  useEffect(()=>{
+  useEffect(() => {
     axios.get(url + "/user/" + membershipId).then(res => {
-      setRS(100-parseInt(res.data.votes));
-      localStorage.setItem('rs', 100-parseInt(res.data.votes));
+      setRS(100 - parseInt(res.data.votes));
+      localStorage.setItem('rs', 100 - parseInt(res.data.votes));
       setAskMembershipId(false);
       setSBD(false)
     })
-  },[membershipId])
+  }, [membershipId])
   useEffect(() => {
     if (!membershipId) {
       setAskMembershipId(true);
@@ -55,7 +56,7 @@ function App() {
   const saveMembershipId = () => {
     setSBD(true);
     const _refine = midInput.split(" ").join("").trim().toUpperCase();
-    
+
     const _ = members.filter(value => {
       if (value["Membership ID"]) {
         // console.log(_refine, String(value["Membership ID"]).split(" ").join("").trim());
@@ -92,8 +93,8 @@ function App() {
     axios.post(url + "/vote", data).then(res => {
       setFilteredPolls(res.data.data);
       setPolls(res.data.data)
-      setRS(100-parseInt(res.data.rs));
-      localStorage.setItem("rs",(100-parseInt(res.data.rs)));
+      setRS(100 - parseInt(res.data.rs));
+      localStorage.setItem("rs", (100 - parseInt(res.data.rs)));
     })
   }
 
@@ -127,19 +128,19 @@ function App() {
 
   const getTopChart = () => {
     let _sorted = polls;
-    let sorted = _sorted.sort((a,b)=> b.voters.length-a.voters.length);
+    let sorted = _sorted.sort((a, b) => b.voters.length - a.voters.length);
     return sorted;
   }
 
   const filterPoll = () => {
-	let searchQuery = searchBar.current.value.replace(" ", "").toUpperCase();
-	setFilteredPolls(() => {
-		let filtered = polls.filter(poll => {
-			let contestant = getName(poll.author).replace(" ", "").toUpperCase();
-			if(contestant.includes(searchQuery)) return true;
-		});
-		return filtered;
-	});
+    let searchQuery = searchBar.current.value.replace(" ", "").toUpperCase();
+    setFilteredPolls(() => {
+      let filtered = polls.filter(poll => {
+        let contestant = getName(poll.author).replace(" ", "").toUpperCase();
+        if (contestant.includes(searchQuery)) return true;
+      });
+      return filtered;
+    });
   }
 
   return (
@@ -152,17 +153,17 @@ function App() {
           <p>Voting open</p>
           <Sentry color="#49b33e" size={24} speed={.5} animating={true} />
         </div>
-		<div className="header_search_box flex">
-			<input type="text" className="search_box" placeholder='Search Contestant' ref={searchBar} 
-				onChange={e => e.target.value ? null : filterPoll()}/>
-			<i className="ri-search-line search_btn flex items-center justify-center" onClick={filterPoll}></i>
-		</div>
+        <div className="header_search_box flex">
+          <input type="text" className="search_box" placeholder='Search Contestant' ref={searchBar}
+            onChange={e => e.target.value ? null : filterPoll()} />
+          <i className="ri-search-line search_btn flex items-center justify-center" onClick={filterPoll}></i>
+        </div>
       </header>
-      <center>
+      {/* <center>
         <h2> {"==>>"} Major Upgrage being carried out</h2>
         <p>Resumes on: Nov 2, 6 AM</p>
-      </center>
-      {/* {showBackDrop && <CustomBackDrop />} 
+      </center> */}
+      {showBackDrop && <CustomBackDrop />}
       {userName && <p className=' px-4 py-1 remaining_notice'>Remaining Vote Count for <b>{userName}</b> :- <span>{remainingSlots}</span></p>}
       {askMembershipId && <dialog className='flex items-center py-2 w-full h-full justify-center z-[100] backdrop:backdrop-blur-sm rounded-md'>
         <div className='flex flex-col gap-[1rem] border border-solid border-[#000] px-4 py-5'>
@@ -177,103 +178,115 @@ function App() {
           />
           <button onClick={() => saveMembershipId()} className='border border-solid bg-primary text-[#fff] py-1'>SAVE</button>
         </div>
-      </dialog>}  
-      { photoActive && <div className='poll_card flex single_poll py-3 px-1'>
-                      <img className='flex-1 w-50' src={photoActive?.url} />
-                      <div className='flex flex-col gap-1 details'>
-                        <span className='photo_id'>{photoActive.id}</span>
-                        <div className='author_section'>
-                        <i className="ri-user-6-fill"></i>
-                        <h2 className='author_name'>{getName(photoActive.author)} <span>{photoActive.author}</span></h2>
-                        </div>
-                        <div className='vote_section'>
-                        <i className="ri-heart-2-fill"></i>
-                        <h2 className='vote_count'>{photoActive.voters.length}<span>Likes</span></h2>
-                        </div>
-                        <h2 className='date_h2'>Uploaded on: {new Date(photoActive.createdAt).toUTCString()}</h2>
-                        <div className='card_footer'>
-                          <button className="vote_button" onClick={() => makeVote(photoActive.id)}>Vote <i className="ri-heart-add-fill"></i> </button>
-                          <button disabled className='votes_count'>
-                            <i className="ri-heart-2-fill"></i>
-                            {photoActive.voters.length}
-                            <Levels />
-                          </button>
-                          <button className='view_btn' onClick={() => location.href = "/"}>
-                            Back<i className="ri-link-unlink-m"></i>
-                          </button>
-                          <button className='copy_link' onClick={()=>{
-                              navigator.clipboard.writeText("https://contest.nagarjunaictclub.com/?photo="+photoActive.id)
-                              toast.success("Link Copied to Clipboard",{
-                                position:"bottom-center",
-                                type:"success"
-                              })
-                              }
-                            }>Copy Link <i className="ri-clipboard-line"></i></button>
-                        </div>
-                      </div>
-                    </div>
-                      }
-      { <button className={`${topChartOn ? 'chart_on': ''} mobile_btn`} onClick={()=>setTCO(!topChartOn)}>{topChartOn ?<span><i className="ri-polaroid-2-line"></i> show all polls</span> :<span><i className="ri-line-chart-line"></i> show top chart</span>}</button>}
+      </dialog>}
+      {photoActive && <div className='poll_card flex single_poll py-3 px-1'>
+        <img className='flex-1 w-50' src={photoActive?.url} />
+        <div className='flex flex-col gap-1 details'>
+          <span className='photo_id'>{photoActive.id}</span>
+          <div className='author_section'>
+            <i className="ri-user-6-fill"></i>
+            <h2 className='author_name'>{getName(photoActive.author)} <span>{photoActive.author}</span></h2>
+          </div>
+          <div className='vote_section'>
+            <i className="ri-heart-2-fill"></i>
+            <h2 className='vote_count'>{photoActive.voters.length}<span>Likes</span></h2>
+          </div>
+          <h2 className='date_h2'>Uploaded on: {new Date(photoActive.createdAt).toUTCString()}</h2>
+          <div className='card_footer'>
+            <button className="vote_button" onClick={() => makeVote(photoActive.id)}>Vote <i className="ri-heart-add-fill"></i> </button>
+            <button disabled className='votes_count'>
+              <i className="ri-heart-2-fill"></i>
+              {photoActive.voters.length}
+              <Levels />
+            </button>
+            <button className='view_btn' onClick={() => location.href = "/"}>
+              Back<i className="ri-link-unlink-m"></i>
+            </button>
+            <button className='copy_link' onClick={() => {
+              navigator.clipboard.writeText("https://contest.nagarjunaictclub.com/?photo=" + photoActive.id)
+              toast.success("Link Copied to Clipboard", {
+                position: "bottom-center",
+                type: "success"
+              })
+            }
+            }>Copy Link <i className="ri-clipboard-line"></i></button>
+          </div>
+        </div>
+      </div>
+      }
+      {<button className={`${topChartOn ? 'chart_on' : ''} mobile_btn`} onClick={() => setTCO(!topChartOn)}>{topChartOn ? <span><i className="ri-polaroid-2-line"></i> show all polls</span> : <span><i className="ri-line-chart-line"></i> show top chart</span>}</button>}
       <div className='poll_container py-3 '>
         {loading ? <div> <CustomBackDrop color={"white"} /> </div> :
           !askMembershipId &&
           <>
-            {!loading && <div className={topChartOn ? 'top_charts active':'top_charts'}>
-                <h2>Top Chart</h2>
-                {getTopChart().map((top,key)=>{
-            
-                  if(top.voters.length>0){
-                    return <div key={key} className='chart_card flex gap-1' onClick={() => location.href += "/?photo=" + top.id}>
-                      <img src={top.url} width={'100'} />
-                      <p className='badge'>{top.voters.length} votes</p>
-                      <div>
+            {!loading && <div className={topChartOn ? 'top_charts active' : 'top_charts'}>
+              <h2>Top Chart</h2>
+              {getTopChart().map((top, key) => {
+
+                if (top.voters.length > 0) {
+                  return <div key={key} className='chart_card flex gap-1' onClick={() => location.href += "/?photo=" + top.id}>
+                    <LazyLoadImage
+                      alt={top.id}
+                      effect="blur"
+                      src={top.url}
+                      width={'100'}
+                       />
+                    {/* <img src={top.url} width={'100'} /> */}
+                    <p className='badge'>{top.voters.length} votes</p>
+                    <div>
                       <span className='photo_id'>{top.id}</span>
                       <p className='author_name'>{top.author}<span>{getName(top.author)}</span></p>
-                     
-                      </div>
+
                     </div>
-                  }
-                })}
-              </div>}
-              
-              <div className='all_polls scrollbar'>
-                {
-                  filteredPolls.map((vlaue, k) => {
-                    return <div key={k} className='poll_card flex flex-col gap-1'>
-                      <img src={vlaue?.url} />
-                      <div className='flex flex-col gap-1'>
-                        <span className='photo_id'>{vlaue.id}</span>
-                        <div className='author_section'>
+                  </div>
+                }
+              })}
+            </div>}
+
+            <div className='all_polls scrollbar'>
+              {
+                filteredPolls.map((vlaue, k) => {
+                  return <div key={k} className='poll_card flex flex-col gap-1'>
+                    {/* <img src={vlaue?.url} /> */}
+                    <LazyLoadImage
+                      alt={vlaue.id}
+                      effect="blur"
+                      src={vlaue.url}
+                    
+                       />
+                    <div className='flex flex-col gap-1'>
+                      <span className='photo_id'>{vlaue.id}</span>
+                      <div className='author_section'>
                         <i className="ri-user-6-fill"></i>
                         <h2 className='author_name'>{getName(vlaue.author)}
-							<span>{vlaue.author}</span>
-						</h2>
-                        </div>
-                        <div className='vote_section'>
+                          <span>{vlaue.author}</span>
+                        </h2>
+                      </div>
+                      <div className='vote_section'>
                         <i className="ri-heart-2-fill"></i>
                         <h2 className='vote_count'>{vlaue.voters.length}<span>Likes</span></h2>
-                        </div>
-                        <h2 className='date_h2'>Uploaded on: {new Date(vlaue.createdAt).toUTCString()}</h2>
-                        <div className='card_footer'>
-                          <button className="vote_button" onClick={() => makeVote(vlaue.id)}>Vote <i className="ri-heart-add-fill"></i> </button>
-                          <button disabled className='votes_count'>
-                            <i className="ri-heart-2-fill"></i>
-                            {vlaue.voters.length}
-                            <Levels />
-                          </button>
-                          <button className='view_btn' onClick={() => location.href = "/?photo=" + vlaue.id}>
-                            View <i className="ri-link-unlink-m"></i>
-                          </button>
-                        </div>
+                      </div>
+                      <h2 className='date_h2'>Uploaded on: {new Date(vlaue.createdAt).toUTCString()}</h2>
+                      <div className='card_footer'>
+                        <button className="vote_button" onClick={() => makeVote(vlaue.id)}>Vote <i className="ri-heart-add-fill"></i> </button>
+                        <button disabled className='votes_count'>
+                          <i className="ri-heart-2-fill"></i>
+                          {vlaue.voters.length}
+                          <Levels />
+                        </button>
+                        <button className='view_btn' onClick={() => location.href = "/?photo=" + vlaue.id}>
+                          View <i className="ri-link-unlink-m"></i>
+                        </button>
                       </div>
                     </div>
-                  })
-                }
-              </div>
-            </>
+                  </div>
+                })
+              }
+            </div>
+          </>
 
         }
-      </div> */}
+      </div>
     </>
   )
 }
