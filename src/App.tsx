880eeddbf6e077af670518ca3eaed227073533f3
SudeepMi <sudeepm0599@gmail.com>
2023-11-07 01:42:27 +0545

    swipper added

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,13 @@ import 'remixicon/fonts/remixicon.css'
 import { toast } from 'react-toastify';
 import { LazyLoadImage } from "react-lazy-load-image-component"
 import 'react-lazy-load-image-component/src/effects/blur.css';
+import Countdown from 'react-countdown';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "swiper/swiper-bundle.css"
+import { Navigation, Scrollbar, A11y, Autoplay, Keyboard } from 'swiper/modules';
+import ProgressBar from "@ramonak/react-progress-bar";
+
+
 
 
 function App() {
@@ -24,6 +31,7 @@ function App() {
   const [showBackDrop, setSBD] = useState(false)
   const [topChartOn, setTCO] = useState(false);
   const searchBar = useRef(null);
+  const [topChart, setTopChart] = useState([]);
 
   let params = new URLSearchParams(window.location.search).get("photo");
 
@@ -33,25 +41,28 @@ function App() {
   const url = "https://backend-poll.onrender.com/api";
   // const url = "http://localhost:3000/api";
 
-  useEffect(() => {
-    axios.get(url + "/user/" + membershipId).then(res => {
-      setRS(100 - parseInt(res.data.votes));
-      localStorage.setItem('rs', 100 - parseInt(res.data.votes));
-      setAskMembershipId(false);
-      setSBD(false)
-    })
-  }, [membershipId])
+
   useEffect(() => {
     if (!membershipId) {
       setAskMembershipId(true);
+    } else {
+      axios.get(url + "/user/" + membershipId).then(res => {
+        setRS(100 - parseInt(res.data.votes));
+        localStorage.setItem('rs', 100 - parseInt(res.data.votes));
+        setAskMembershipId(false);
+        setSBD(false)
+      })
+      axios.get(url + "/all-polls").then(res => {
+        setPolls(res.data.data)
+        setFilteredPolls(res.data.data)
+        setLoading(false);
+      })
     }
-
-    axios.get(url + "/all-polls").then(res => {
-      setPolls(res.data.data)
-      setFilteredPolls(res.data.data)
-      setLoading(false);
-    })
   }, [membershipId])
+
+  useEffect(() => {
+    getTopChart();
+  }, [])
 
   const saveMembershipId = () => {
     setSBD(true);
@@ -95,7 +106,7 @@ function App() {
       setPolls(res.data.data)
       setRS(100 - parseInt(res.data.rs));
       localStorage.setItem("rs", (100 - parseInt(res.data.rs)));
-    })
+    }).catch(er=>toast(er.response.data.message,{type:"error",position:"bottom-center"}))
   }
 
   const getName = (id) => {
@@ -127,9 +138,11 @@ function App() {
   }
 
   const getTopChart = () => {
-    let _sorted = polls;
-    let sorted = _sorted.sort((a, b) => b.voters.length - a.voters.length);
-    return sorted;
+    axios.get(url + "/ranks").then(res => {
+      let _data = res.data;
+      _data = _data.sort((a, b) => b.popularity - a.popularity)
+      setTopChart(_data)
+    })
   }
 
   const filterPoll = () => {
@@ -146,12 +159,13 @@ function App() {
   return (
     <>
       <header className='flex items-center justify-between'>
-        <div className='heading_container'>
+        {/* <div className='heading_container'>
           <h2>Nagarjuna ICT Club - Photography Contest</h2>
-        </div>
+        </div> */}
         <div className="header_search_area flex items-center gap-[1rem]">
           <p>Voting open</p>
           <Sentry color="#49b33e" size={24} speed={.5} animating={true} />
+          <Countdown date={Date.now() + (86400 / 24) * 10 * 1000} />
         </div>
         <div className="header_search_box flex">
           <input type="text" className="search_box" placeholder='Search Contestant' ref={searchBar}
@@ -179,41 +193,7 @@ function App() {
           <button onClick={() => saveMembershipId()} className='border border-solid bg-primary text-[#fff] py-1'>SAVE</button>
         </div>
       </dialog>}
-      {photoActive && <div className='poll_card flex single_poll py-3 px-1'>
-        <img className='flex-1 w-50' src={photoActive?.url} />
-        <div className='flex flex-col gap-1 details'>
-          <span className='photo_id'>{photoActive.id}</span>
-          <div className='author_section'>
-            <i className="ri-user-6-fill"></i>
-            <h2 className='author_name'>{getName(photoActive.author)} <span>{photoActive.author}</span></h2>
-          </div>
-          <div className='vote_section'>
-            <i className="ri-heart-2-fill"></i>
-            <h2 className='vote_count'>{photoActive.voters.length}<span>Likes</span></h2>
-          </div>
-          <h2 className='date_h2'>Uploaded on: {new Date(photoActive.createdAt).toUTCString()}</h2>
-          <div className='card_footer'>
-          {remainingSlots > 0 &&  <button className="vote_button" onClick={() => makeVote(vlaue.id)}>Vote <i className="ri-heart-add-fill"></i> </button> }
-            <button disabled className='votes_count'>
-              <i className="ri-heart-2-fill"></i>
-              {photoActive.voters.length}
-              <Levels />
-            </button>
-            <button className='view_btn' onClick={() => location.href = "/"}>
-              Back<i className="ri-link-unlink-m"></i>
-            </button>
-            <button className='copy_link' onClick={() => {
-              navigator.clipboard.writeText("https://contest.nagarjunaictclub.com/?photo=" + photoActive.id)
-              toast.success("Link Copied to Clipboard", {
-                position: "bottom-center",
-                type: "success"
-              })
-            }
-            }>Copy Link <i className="ri-clipboard-line"></i></button>
-          </div>
-        </div>
-      </div>
-      }
+
       {<button className={`${topChartOn ? 'chart_on' : ''} mobile_btn`} onClick={() => setTCO(!topChartOn)}>{topChartOn ? <span><i className="ri-polaroid-2-line"></i> show all polls</span> : <span><i className="ri-line-chart-line"></i> show top chart</span>}</button>}
       <div className='poll_container py-3 '>
         {loading ? <div> <CustomBackDrop color={"white"} /> </div> :
@@ -221,22 +201,21 @@ function App() {
           <>
             {!loading && <div className={topChartOn ? 'top_charts active' : 'top_charts'}>
               <h2>Top Chart</h2>
-              {getTopChart().map((top, key) => {
-
-                if (top.voters.length > 0) {
-                  return <div key={key} className='chart_card flex gap-1' onClick={() => location.href += "/?photo=" + top.id}>
-                    <LazyLoadImage
-                      alt={top.id}
-                      effect="blur"
-                      src={top.url}
-                      width={'100'}
-                       />
-                    {/* <img src={top.url} width={'100'} /> */}
-                    <p className='badge'>{top.voters.length} votes</p>
+              {topChart.map((top, key) => {
+                if (top.votes > 0) {
+                  return <div key={key} className='chart_card gap-1'>
+                    <p className='badge'>{top.votes} votes</p>
                     <div>
-                      <span className='photo_id'>{top.id}</span>
                       <p className='author_name'>{top.author}<span>{getName(top.author)}</span></p>
-
+                      <span className='photo_id'>Popularity</span>
+                      <ProgressBar
+                        completed={top.popularity}
+                        maxCompleted={101}
+                        customLabel={String(top.popularity)}
+                        baseBgColor="#bfbfbf"
+                        height='15px'
+                        bgColor='#e92b3d'
+                      />
                     </div>
                   </div>
                 }
@@ -252,8 +231,8 @@ function App() {
                       alt={vlaue.id}
                       effect="blur"
                       src={vlaue.url}
-                    
-                       />
+
+                    />
                     <div className='flex flex-col gap-1'>
                       <span className='photo_id'>{vlaue.id}</span>
                       <div className='author_section'>
@@ -264,11 +243,11 @@ function App() {
                       </div>
                       <div className='vote_section'>
                         <i className="ri-heart-2-fill"></i>
-                        <h2 className='vote_count'>{vlaue.voters.length}<span>Likes</span></h2>
+                        <h2 className='vote_count'>{vlaue.voters}<span>Likes</span></h2>
                       </div>
                       <h2 className='date_h2'>Uploaded on: {new Date(vlaue.createdAt).toUTCString()}</h2>
                       <div className='card_footer'>
-                      {remainingSlots > 0 &&  <button className="vote_button" onClick={() => makeVote(vlaue.id)}>Vote <i className="ri-heart-add-fill"></i> </button> }
+                        {<button className="vote_button" onClick={() => makeVote(vlaue.id)}>Vote <i className="ri-heart-add-fill"></i> </button>}
                         <button disabled className='votes_count'>
                           <i className="ri-heart-2-fill"></i>
                           {vlaue.voters.length}
@@ -283,6 +262,112 @@ function App() {
                 })
               }
             </div>
+            <Swiper
+              modules={[Navigation, Scrollbar, A11y, Autoplay, Keyboard]}
+              spaceBetween={50}
+              slidesPerView={1}
+              watchSlidesProgress={true}
+              autoplay={{
+                delay: 3000,
+                pauseOnMouseEnter: true,
+                stopOnLastSlide: false,
+              }}
+              loop={true}
+              navigation
+              scrollbar={{ draggable: true }}
+              onSwiper={(swiper) => console.log(swiper)}
+              onSlideChange={() => console.log('slide change')}
+              style={{ maxWidth: "100vw", padding: "10px", maxHeight: "90vh" }}
+              effect='cube'
+              className='custom_swiper'
+            >
+              {photoActive && <SwiperSlide><div className='poll_card flex single_poll py-3 px-1'>
+                <LazyLoadImage
+                  alt={photoActive.id}
+                  effect="blur"
+                  src={photoActive.url}
+
+                />
+                {/* <img className='flex-1 w-50' src={photoActive?.url} /> */}
+                <div className='flex flex-col gap-1 details'>
+                  <span className='photo_id'>{photoActive.id}</span>
+                  <div className='author_section'>
+                    <i className="ri-user-6-fill"></i>
+                    <h2 className='author_name'>{getName(photoActive.author)} <span>{photoActive.author}</span></h2>
+                  </div>
+                  <div className='vote_section'>
+                    <i className="ri-heart-2-fill"></i>
+                    <h2 className='vote_count'>{photoActive.voters}<span>Likes</span></h2>
+                  </div>
+                  <h2 className='date_h2'>Uploaded on: {new Date(photoActive.createdAt).toUTCString()}</h2>
+                  <div className='card_footer'>
+                    {remainingSlots > 0 && <button className="vote_button" onClick={() => makeVote(vlaue.id)}> <i className="ri-heart-add-fill"></i> </button>}
+                    <button disabled className='votes_count'>
+                      <i className="ri-heart-2-fill"></i>
+                      {photoActive.voters}
+                      <Levels />
+                    </button>
+                    <button className='view_btn' onClick={() => location.href = "/"}>
+                      Back<i className="ri-link-unlink-m"></i>
+                    </button>
+                    <button className='copy_link' onClick={() => {
+                      navigator.clipboard.writeText("https://contest.nagarjunaictclub.com/?photo=" + photoActive.id)
+                      toast.success("Link Copied to Clipboard", {
+                        position: "bottom-center",
+                        type: "success"
+                      })
+                    }
+                    }>Copy Link <i className="ri-clipboard-line"></i></button>
+                  </div>
+                </div>
+              </div>
+              </SwiperSlide>
+              }
+              {
+                filteredPolls.map((vlaue, k) => {
+                  return (<SwiperSlide key={k}>
+                    <div className='poll_card flex flex-col gap-1'>
+                      <LazyLoadImage
+                        alt={vlaue.id}
+                        effect="blur"
+                        src={vlaue.url}
+
+                      />
+                      <div className='flex flex-col gap-1'>
+                        <span className='photo_id'>{vlaue.id}</span>
+                        <div className='author_section'>
+                          <i className="ri-user-6-fill"></i>
+                          <h2 className='author_name'>{getName(vlaue.author)}
+                            <span>{vlaue.author}</span>
+                          </h2>
+                        </div>
+                        <div className='vote_section'>
+                          <i className="ri-heart-2-fill"></i>
+                          <h2 className='vote_count'>{vlaue.voters}<span>Likes</span></h2>
+                          <h2 className='date_h2'>Uploaded on: {new Date(vlaue.createdAt).toUTCString()}</h2>
+                        </div>
+
+                        <div className='card_footer'>
+
+                          {<button className="vote_button" onClick={() => makeVote(vlaue.id)}><i className="ri-heart-add-fill"></i> </button>}
+                          {<button className="vote_button black" onClick={() => {
+                            navigator.clipboard.writeText("https://contest.nagarjunaictclub.com/?photo=" + vlaue.id)
+                            toast.success("Link Copied to Clipboard", {
+                              position: "bottom-center",
+                              type: "success"
+                            })
+                          }
+                          }><i className="ri-share-fill"></i> </button>}
+
+
+
+                        </div>
+                      </div>
+                    </div>
+                  </SwiperSlide>)
+                })
+              }
+            </Swiper>
           </>
 
         }
