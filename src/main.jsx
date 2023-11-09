af3d68f28b423403956dcfcec92472f065b541a6
Suraj Gaire <eriag321@gmail.com>
2023-11-09 22:01:39 +0545

    minor change

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
